ede78890a87c6104301066f010853125ba1309de
danybeltran <josuebeltran_2010@hotmail.com>
2024-05-12 20:31:13 -0600

    feat(RenderList): Adds the RenderList component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -77,3 +77,13 @@ export function WithLayout<L>({
     </SLayout>
   )
 }
+
+export function RenderList<T>({
+  data,
+  render,
+}: {
+  data: T[]
+  render: (item: T, i: number, arr: T[]) => React.ReactNode
+}) {
+  return data.map(render)
+}
